edb8582a5706550ca5fda053b7106e700afa92c1
3ibsi69 <a.abbassi21306@pi.tn>
2024-08-26 14:19:03 +0100

    final

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "FullStack JavaScript Developer",
   description:
-    "My name is ABBASSI Ahmed. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+    "My name is Ahmed Abbassi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "a.abbassipro@gmail.com",
   phone: "+216 23 631   633",
   address: "Tunis, Tunisia",
